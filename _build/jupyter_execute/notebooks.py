adb4b4573fd0b4fabb4a3c057a6dcad052da8ac8
PhiSpel <philipp.spelten@web.de>
2023-02-08 12:18:28 +0100

    added build

---
+++ _build/jupyter_execute/notebooks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # dropdown admonition bug demo
+# 
+# When I include an image in a dropdown admonition, it only collapses down to the fixed image size.
+
+# ````{admonition} Click here!
+# :class: dropdown
+# Here is an image. The dropdown menu only collapsed by the height of the test.
+# ```{image} image.png
+# :width: 300px
+# ```
+# Here is some more text for demonstration.
+# 
+# And some more text for demonstration.
+# 
+# And some more text for demonstration.
+# 
+# And some more text for demonstration.
+# 
+# And some more text for demonstration.
+# ````
